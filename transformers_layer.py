7150777a65247be960ce7be6a2a0e308421c66ef
Sukhvansh2004 <120120146+Sukhvansh2004@users.noreply.github.com>
2023-09-15 00:50:43 +0530

    Update transformers_layer.py

--- transformers_layer.py
+++ transformers_layer.py
@@ -3,60 +3,6 @@ import torch.nn as nn
 from torch.nn import functional as F
 import math
 
-"""
-This file defines layer types that are commonly used for transformers.
-"""
-
-class PositionalEncoding(nn.Module):
-    """
-    Encodes information about the positions of the tokens in the sequence. In
-    this case, the layer has no learnable parameters, since it is a simple
-    function of sines and cosines.
-    """
-    def __init__(self, embed_dim, dropout=0.1, max_len=5000):
-        """
-        Construct the PositionalEncoding layer.
-
-        Inputs:
-         - embed_dim: the size of the embed dimension
-         - dropout: the dropout value
-         - max_len: the maximum possible length of the incoming sequence
-        """
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-        assert embed_dim % 2 == 0
-        pe = torch.zeros(1, max_len, embed_dim)
-       
-        
-        row = torch.arange(max_len).unsqueeze(1)
-        col_sin = torch.arange(0, embed_dim , 2).unsqueeze(0)
-        col_cos = torch.arange(1, embed_dim , 2).unsqueeze(0)
-        pe[ 0, :, col_sin[0]] = torch.sin((row) * 10000**(-col_sin/embed_dim))
-        pe[ 0, :, col_cos[0]] = torch.cos((row) * 10000**(-(col_cos-1)/embed_dim))
-            
-        pass
-        
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        """
-        Element-wise add positional embeddings to the input sequence.
-
-        Inputs:
-         - x: the sequence fed to the positional encoder model, of shape
-              (N, S, D), where N is the batch size, S is the sequence length and
-              D is embed dim
-        Returns:
-         - output: the input sequence + positional encodings, of shape (N, S, D)
-        """
-        N, S, D = x.shape
-        output = torch.empty((N, S, D))
-        
-        output = self.dropout(x + self.pe[:,:S,:D]) 
-        pass
-    
-        return output
-
 
 class MultiHeadAttention(nn.Module):
     """
@@ -66,13 +12,9 @@ class MultiHeadAttention(nn.Module):
       # self-attention
       data = torch.randn(batch_size, sequence_length, embed_dim)
       self_attn_output = attn(query=data, key=data, value=data)
-
-      # attention using two inputs
-      other_data = torch.randn(batch_size, sequence_length, embed_dim)
-      attn_output = attn(query=data, key=other_data, value=other_data)
     """
 
-    def __init__(self, dim, num_heads, window_size, dropout=0.1):
+    def __init__(self, dim, num_heads, window_size, dropout=0.0):
         """
         Construct a new MultiHeadAttention layer.
 
@@ -88,55 +30,33 @@ class MultiHeadAttention(nn.Module):
 
         self.n_head = num_heads
         self.head_dim = (dim[0])// self.n_head
-        
-        self.key = nn.Linear(dim[0], self.head_dim)
-        self.query = nn.Linear(dim[0], self.head_dim)
-        self.value = nn.Linear(dim[0], self.head_dim)
-        self.proj = nn.Linear(dim[0], self.head_dim)
+        self.window = window_size[0] * window_size[1]
+        self.local_window = (dim[1] * dim[2]) % (window_size[0] * window_size[1])
         
         self.attn_drop = nn.Dropout(dropout)
-
-
-    def forward(self, query, key, value, attn_mask=None):
-        """
-        Calculate the masked attention output for the provided data, computing
-        all attention heads in parallel.
-
-        In the shape definitions below, N is the batch size, S is the total no. of features, T is the target sequence length, and E is the window
-        dimension.
-
-        Inputs:
-        - query: Input data to be used as the query, of shape (N, S, E)
-        - key: Input data to be used as the key, of shape (N, T, E)
-        - value: Input data to be used as the value, of shape (N, T, E)
-        - attn_mask: Array of shape (S, T) where mask[i,j] == 0 indicates token
-          i in the source should not influence token j in the target.
-
-        Returns:
-        - output: Tensor of shape (N, S, E) giving the weighted combination of
-          data in value according to the attention weights calculated using key
-          and query.
-        """
-        N, S, E = query.shape
-        N, T, E = value.shape
         
-        output = torch.empty((N, S, E))
+        self.key = nn.Linear(dim[0], dim[0])
+        self.query = nn.Linear(dim[0], dim[0])
+        self.value = nn.Linear(dim[0], dim[0])
+        self.B = torch.nn.init.Tensor(self.window, dim[0]).requires_grad_()
+
+    def forward(self, x):
+       
+        shape = x.shape
+        x = x.reshape(shape[0], self.local_window, self.window, shape[1])
         
         key = self.key(key)
         query = self.query(query)
         value = self.value(value)
      
-        Q = query.view(N, S, self.n_head, self.head_dim).moveaxis(1,2)
-        K = key.view(N, T, self.n_head, self.head_dim).moveaxis(1,2)
-        V = value.view(N, T, self.n_head, self.head_dim).moveaxis(1,2)
+        Q = query.view(query.shape[0],  query.shape[1], query.shape[2], self.n_head, self.head_dim).moveaxis((2,3),(3,2))
+        K = key.view(query.shape[0],  query.shape[1], query.shape[2], self.n_head, self.head_dim).moveaxis((2,3),(3,2))
+        V = value.view(query.shape[0],  query.shape[1], query.shape[2], self.n_head, self.head_dim).moveaxis((2,3),(3,2))
         
-        scores = torch.matmul(Q, K.transpose(2,3))/(self.head_dim**(1/2))
-        if attn_mask is not None:
-          scores = scores.masked_fill(attn_mask==0, float(-torch.inf))
-          pass
+        scores = torch.matmul(Q, K.transpose(3,4))/(self.head_dim**(1/2)) + self.B
+        
         probs = F.softmax(scores,dim=-1)
-        output = self.proj(torch.matmul(self.attn_drop(probs), V).moveaxis(1,2).reshape(N, S, E))
-
+        output = (torch.matmul(self.attn_drop(probs), V).moveaxis((2,3),(3,2)).reshape(shape))
 
         return output
       
