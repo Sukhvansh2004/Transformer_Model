04c176c8fa2ab0b3ca158e987102be0179def85e
Sukhvansh2004 <120120146+Sukhvansh2004@users.noreply.github.com>
2023-09-15 19:22:23 +0530

    Code dump

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from feature_extractor import *
 import numpy as np
 
-class SWINIR(nn.module):
-    def __init__(self, dim, RSTB_nos = 6, STL_nos = 6, window_size = (8, 8), channel_nos = 180, attn_head = 6):
+class SWINIR(nn.Module):
+    def __init__(self, dim: tuple, RSTB_nos = 6, STL_nos = 6, window_size = (8, 8), channel_nos = 180, attn_head = 6):
     
         """Implementation of the SWINIR Model:
         
@@ -17,7 +17,7 @@ class SWINIR(nn.module):
         - attn_head: No. of attention heads for the MSA"""
         
         super().__init__()
-        self.extractor = feature(input_dimension = dim, output_dimension = (channel_nos, dim[1], dim[2]), n = np.ones(RSTB_nos) * STL_nos, heads = np.ones((RSTB_nos, STL_nos)) * attn_head, window_size = window_size)
+        self.extractor = feature(input_dimension = dim, output_dimension = (channel_nos, dim[1], dim[2]), n = np.ones(RSTB_nos, dtype=int) * STL_nos, heads = np.ones((RSTB_nos, STL_nos), dtype=int) * attn_head, window_size = window_size)
         self.HQ_Reconstruction = nn.Sequential(
             nn.ConvTranspose2d(channel_nos, channel_nos//4, 4, 2, padding=1),
             nn.Conv2d(channel_nos//4, channel_nos//4, 3, padding="same"),
